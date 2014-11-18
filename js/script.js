85402a9e188ee127edc8b79b322afa5d4441024a
unknown <cyruslacang@yahoo.com>
2014-11-18 15:31:04 +0800

    product details touch bug fix

--- js/script.js
+++ js/script.js
@@ -681,17 +681,17 @@ $(document).ready(function($) {
 		$('.mob-promo-hide').parent().prev().hide();
 	}
 	if ($(window).width() < 1025){
-		$(document).on('mouseover', '.zoomContainer', function(event) {
+		$(document).on('mouseenter', '.zoomContainer', function(event) {
 			$('.product-doverlay, .zoomWindow').show();
 			$( ".zoomWindow" ).append( "<span class='p-dialog-close-btn'>x</span>" );
 			/* Act on the event */
 		});
-		$(document).on('mousedown', '.product-doverlay, .p-dialog-close-btn', function(event) {
+		$(document).on('mouseenter', '.product-doverlay, .p-dialog-close-btn', function(event) {
 			$('.product-doverlay, .zoomWindow').hide();
 			$( ".p-dialog-close-btn" ).remove();
 			/* Act on the event */
 		});
-		$(document).on('mousedown', '.zoomWindow', function(event) {
+		$(document).on('mouseenter', '.zoomWindow', function(event) {
 			event.stopPropagation();
 		});
 
