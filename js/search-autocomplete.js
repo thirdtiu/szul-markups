7802ae6cd06cbc0d92eeb36f6805cb51914256cb
rrsdr84 <rene@coderepubliq.com>
2015-07-13 18:12:59 +0800

    szul searchform autocomplete

---
+++ js/search-autocomplete.js
@@ -0,0 +1,22 @@
+$(function(){
+  var currencies = [
+    { value: 'Diamond Sample 1', data: 'DMND1' },
+    { value: 'Diamond Sample 2', data: 'DMND2' },
+    { value: 'Diamond Sample 3', data: 'DMND3' },
+    { value: 'Diamond Sample 4', data: 'DMND4' },
+    { value: 'Diamond Sample 5', data: 'DMND5' },
+
+   
+  ];
+  
+  // setup autocomplete function pulling from currencies[] array
+  $('#autocomplete').autocomplete({
+    lookup: currencies,
+    onSelect: function (suggestion) {
+      var thehtml = '<strong>Currency Name:</strong> ' + suggestion.value + ' <br> <strong>Symbol:</strong> ' + suggestion.data;
+      $('#outputcontent').html(thehtml);
+    }
+  });
+  
+
+});
